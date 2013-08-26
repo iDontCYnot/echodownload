5aff6ad28194eebc776e10a6a80b22d9793fc2cb
Joshua <Joshua@Joshuas-MacBook-Pro.local>
2013-08-26 23:32:27 +0800

    remove console logs

--- backgroundEchoCenter.js
+++ backgroundEchoCenter.js
@@ -1,10 +1,10 @@
 //listen for completed XHR request
 chrome.webRequest.onCompleted.addListener(function(info) {
 	//check that request was for lecture meta
-	console.log(info);
+	//console.log(info);
   if(info.url.toString().search('loadDetailsSuccess') > -1){
 	  //extract the uuid from the request url
-  	  console.log("RequestDidComplete");
+  	  //console.log("RequestDidComplete");
 	  var urlString = info.url.toString().split('?')[0];
 	  if(!urlString) return;
 
@@ -22,11 +22,11 @@ chrome.webRequest.onCompleted.addListener(function(info) {
 	  
 	  //clip uuid from url string
 	  var uuid = urlString.substring(uuidRangeStart, uuidRangeEnd);
-	  console.log(uuid);
-	  console.log(info.tabId);
+	  //console.log(uuid);
+	  //console.log(info.tabId);
 	  //inform the content script and send uuid
   	  chrome.tabs.sendMessage(info.tabId, {uuid: uuid, url: urlString}, function(message){
-		 console.log(message);
+		 //console.log(message);
   	  });  
   }
 },
