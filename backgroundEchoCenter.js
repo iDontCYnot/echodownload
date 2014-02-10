3c6722f1ab11443ed8738b03f397283f4e2a977f
Joshua Kulesza <joshuakulesza@hotmail.com>
2014-02-10 14:29:33 +1100

    cleaned up code dramatically, may have broken something

--- backgroundEchoCenter.js
+++ backgroundEchoCenter.js
@@ -1,16 +1,12 @@
-//listen for completed XHR request
+//listen for completed XHR
 chrome.webRequest.onCompleted.addListener(function(info) {
 	//check that request was for lecture meta
-	//console.log(info);
-  if(info.url.toString().search('loadDetailsSuccess') > -1){
-	  //extract the uuid from the request url
-  	  //console.log("RequestDidComplete");
-	  var urlString = info.url.toString().split('?')[0];
-	  if(!urlString) return;
-	  //inform the content script and send uuid
-  	  chrome.tabs.sendMessage(info.tabId, {url: urlString}, function(message){
-		 //console.log(message);
-  	  });  
+  	if(info.url.toString().search('loadDetailsSuccess') > -1){
+		//extract the uuid from the request url
+		var urlString = info.url.toString().split('?')[0];
+		if(urlString == null) return;
+		//inform the content script and send uuid
+	  	chrome.tabs.sendMessage(info.tabId, {url: urlString}, null);  
   }
 },
 { urls: ["http://*/ecp/api/*", "http://*/ess/client/api/*"] });
--- contentEchoCenter.js
+++ contentEchoCenter.js
@@ -5,74 +5,46 @@ chrome.extension.onMessage.addListener( processMessage );
 * A method used to process a message sent from the background task
 */
 function processMessage(request, sender, sendResponse) {
-  // change that url exists
-  if (request.url){
-      // get json data for lecture
-	  xmlhttp=new XMLHttpRequest();
-	  xmlhttp.open("GET", request.url,false);
-	  xmlhttp.send();
-	  // parse response
-	  var data = JSON.parse(xmlhttp.responseText);
-	  var title = data.presentation.title;
-	  var uuid  = data.presentation.uuid;
-	  var date  = data.presentation.startTime;
-	  // extract date, time
-	  var tz_regex = /[+-]\d{2}:\d{2}/i
-	  var dt = moment(date.replace(tz_regex, ''));
-	  if(!dt.isValid()) return;
-	  // grab the right click text element
-	  // if element is empty downloads are disabled - otherwise we can drop out
-	  var rightClickTextElement = $(".right-click-text").first();
-	  if(rightClickTextElement){
-		  // set right click text
-		  rightClickTextElement.html(null);
-	  } 
-	  // grab meta data container to place download links
-	  var lectureMeta = $(".info-meta").last();	  
-	  // check that nothing went wrong
-	  if(lectureMeta){
-		  // generate links
-		  var host = request.url.split(/(ess|ecp)/)[0];
-		  if(!host) return;
-		  // console.log(host);
-		  var presentation = getURL( dt, host );
-		  if(!presentation) return; //break if something not right
-		  presentation += uuid;
-		  // console.log(presentation);
-		  var fname = getName( dt, title );
-		  if(!fname) return; //break if something went wrong
-		  // console.log(fname);
-		  var afile = presentation + "/audio.mp3";
-		  console.log(afile);
-		  var vfile = presentation + "/audio-vga.m4v";	  
-		  // generate DOM data
-		  var heading = "<div class=\"info-key\">Downloads</div>";
-		  var aelement = "<div class=\"info-value\"><a href=" + afile + " download=\"" + fname + ".mp3\" title=\"Listen to MP3 Audio File\">Audio File</a></div>";
-		  var velement = "<div class=\"info-value\"><a href=" + vfile + " download=\"" + fname + ".m4v\" title=\"Watch M4V Video or Screen File\">Video File</a></div>";
-		  // add DOM elements to page
-		  lectureMeta.html(heading + aelement + "<br>" + velement);	
-		  // tell background that its been done
-	  } else {
-		  // console.log("lectureMeta IS_NULL");
-	  }
-	}
-}
-
-/**
-* Uses the recording title in order to name the download file
-*/
-function getName( dt, title ){
-	//return course and recording date
-	return title + " - " + dt.format("MMM Do")
-}
-
-/**
-* Uses the week title to form a valid download url using the date.
-*/
-function getURL( dt, host ){
-	// console.log("Creating url with " + dt);
-	var head = host + "echocontent/";
-	console.log("url ending " + head + dt.format("YYWW[/]E[/]"));
-	// console.log("day: "+ dt.format("dd") + " number: "+dt.format("E"));
-	return head + dt.format("YYWW[/]E[/]");
+	// get json data for lecture
+  	xmlhttp=new XMLHttpRequest();
+	xmlhttp.open("GET", request.url,false);
+	xmlhttp.send();
+	var data = JSON.parse(xmlhttp.responseText);
+	// parse response
+	var title = data.presentation.title;
+	var uuid  = data.presentation.uuid;
+	var date  = data.presentation.startTime;
+	// remove timezone from timestamp
+	var tz_regex = /[+-]\d{2}:\d{2}/i
+	var tstamp = moment(date.replace(tz_regex, ''));
+	if(!tstamp.isValid()) return;
+	// grab the right click text element
+	// if element is empty downloads are disabled - otherwise we can drop out
+	var rightClickTextElement = $(".right-click-text").first();
+	// set right click text to null
+	if(rightClickTextElement != null)
+		rightClickTextElement.html(null);
+	// grab meta data container to place download links
+	var lectureMeta = $(".info-meta").last();	  
+	// check that nothing went wrong
+	if(lectureMeta == null) return;
+	// get host URL
+	var host = request.url.split( /(ess|ecp)/ )[0];
+	if(host == null) return;
+	// Media URL beginning
+	var presentation = host + tstamp.format("[echocontent/]YYWW[/]E[/]") + uuid;
+	if(!presentation) return;
+	// filename
+	var fname = title + tstamp.format(" [-] MMM Do");
+	if(!fname) return; //break if something went wrong
+	// make URL to file
+	var afile = presentation + "/audio.mp3";
+	console.log(afile);
+	var vfile = presentation + "/audio-vga.m4v";	  
+	// generate DOM data
+	var heading = "<div class=\"info-key\">Downloads</div>";
+	var aelement = "<div class=\"info-value\"><a href=" + afile + " download=\"" + fname + ".mp3\" title=\"Listen to MP3 Audio File\">Audio File</a></div>";
+	var velement = "<div class=\"info-value\"><a href=" + vfile + " download=\"" + fname + ".m4v\" title=\"Watch M4V Video or Screen File\">Video File</a></div>";
+	// add DOM elements to page
+	lectureMeta.html(heading + aelement + "<br>" + velement);	
 }
