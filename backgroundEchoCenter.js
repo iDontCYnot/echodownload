ccc5542187dd25ceee613699d480fa22f28ce3cf
Joshua Kulesza <joshuakulesza@hotmail.com>
2014-02-10 14:36:43 +1100

    regex for request url

--- backgroundEchoCenter.js
+++ backgroundEchoCenter.js
@@ -3,7 +3,8 @@ chrome.webRequest.onCompleted.addListener(function(info) {
 	//check that request was for lecture meta
   	if(info.url.toString().search('loadDetailsSuccess') > -1){
 		//extract the uuid from the request url
-		var urlString = info.url.toString().split('?')[0];
+		var dl_regex = /^details.json/i
+		var urlString  info.url.toString().firstMatch( dl_regex );
 		if(urlString == null) return;
 		//inform the content script and send uuid
 	  	chrome.tabs.sendMessage(info.tabId, {url: urlString}, null);  
