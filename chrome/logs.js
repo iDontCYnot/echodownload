e5123e5885726585b5e8c0f70c8b3b3e6f20addc
Joshua Kulesza <joshuakulesza@hotmail.com>
2014-04-27 21:28:19 +1000

    remove boolean check in debug. make removes logs anyway

--- chrome/logs.js
+++ chrome/logs.js
@@ -1,17 +1,13 @@
-DEBUG = true;
-
 /**
 * Log to the console
 **/
 function log(message){
-	if(DEBUG)
-		console.log(message);
+	console.log(message);
 }
 
 /**
 * Log to the error console
 **/
 function error(message){
-	if(DEBUG)
-		console.error(message);
+	console.error(message);
 }
